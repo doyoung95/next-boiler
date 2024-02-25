ee3ef3e2ef73ed2d644f7b97bfac664913ee1796
doyoung95 <satpray73@gmail.com>
2024-02-25 16:10:15 +0900

    chunk: 보일러 플레이트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<ThemeProvider theme={{}}>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
